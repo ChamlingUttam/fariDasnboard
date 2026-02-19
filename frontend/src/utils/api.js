70f41f7073863b0dc7c3fe795b8e75b34d11dc29
ChamlingUttam <110346114+ChamlingUttam@users.noreply.github.com>
2026-02-19 17:08:14 +0545

    Update API base URL to production server

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL:"https://localhost:4000/api",
+    baseURL:"https://faridasnboard.onrender.com",
     withCredentials:true,
 })
